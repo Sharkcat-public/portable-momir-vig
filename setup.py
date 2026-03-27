a454b225f5ed4f36bfcfff02c1db886cdea9f545
Ellie <erko.cernaj@gmail.com>
2026-03-27 18:49:33 +0100

    stream json

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import os
 from PIL import Image
 from io import BytesIO
 import json
+from urllib import request
+import ijson
 
 # get bulk data (metadata about all bulk data jsons)
 # https://scryfall.com/docs/api/bulk-data
@@ -17,19 +19,38 @@ for data in bulk_data['data']:
     if data['name'] == 'Oracle Cards':
         oracle_cards_uri = data['download_uri']
         break
-        
 
+stream_cards = request.urlopen(oracle_cards_uri)
+parser = ijson.parse(stream_cards)
+i = 0
+for card in ijson.items(parser, 'item'):
+    if i == 10:
+        break
+    if 'Creature' in card['type_line']:
+        print(f"Adding {card['name']}")
+        converted_mana_cost = int(card['cmc'])
+        img_uri = card['image_uris']['normal']
+        card_path = os.path.join("cards", str(converted_mana_cost), card['name']+'.jpg')
+        os.makedirs(os.path.dirname(card_path), exist_ok=True)
+        resp = requests.get(img_uri)
+        resp.raise_for_status()
+        img = Image.open(BytesIO(resp.content))
+        img = img.convert('L').resize((360,int((360/img.width)*img.height)))
+        img.save(card_path)
+    i = i + 1
+
+"""
 print("Getting oracle cards")
-oracle_cards = None
-if os.path.exists("oracle_cards.json"):
-    with open("oracle_cards.json", 'r') as f:
-        oracle_cards = json.load(f)
-else:
-    resp = requests.get(oracle_cards_uri)
-    resp.raise_for_status()
-    oracle_cards = resp.json()
-    with open("oracle_cards.json", 'w') as f:
-        json.dump(oracle_cards, f)
+if not os.path.exists("oracle_cards.json"):
+    with requests.get(oracle_cards_uri) as r:
+        resp = requests.get(oracle_cards_uri)
+        resp.raise_for_status()
+        oracle_cards = resp.json()
+        with open("oracle_cards.json", 'w') as f:
+            json.dump(oracle_cards, f)
+with open("oracle_cards.json", 'r') as f:
+    oracle_cards = json.load(f)
+
 
 for card in oracle_cards[:50]:
     if 'Creature' in card['type_line']:
@@ -43,6 +64,4 @@ for card in oracle_cards[:50]:
         img = Image.open(BytesIO(resp.content))
         img = img.convert('L').resize((360,int((360/img.width)*img.height)))
         img.save(card_path)
-        
-        
-        
+"""
