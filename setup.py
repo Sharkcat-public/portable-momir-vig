93e7fd8eb11c9825212e0fd06abca9b47da6ec57
Ellie <erko.cernaj@gmail.com>
2026-03-27 23:14:27 +0100

    exclude tokens

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 from PIL import Image
 from io import BytesIO
 from urllib import request
+# thank you ijson https://github.com/ICRAR/ijson
 import ijson
 
 # get bulk data (metadata about all bulk data jsons)
@@ -25,7 +26,7 @@ i = 0
 for card in ijson.items(parser, 'item'):
     if i == 10:
         break
-    if 'Creature' in card['type_line']:
+    if ('Creature' in card['type_line']) and ('Token' not in card['type_line']):
         print(f"Adding {card['name']}")
         converted_mana_cost = int(card['cmc'])
         img_uri = card['image_uris']['normal']
@@ -37,30 +38,3 @@ for card in ijson.items(parser, 'item'):
         img = img.convert('L').resize((360,int((360/img.width)*img.height)))
         img.save(card_path)
     i = i + 1
-
-"""
-print("Getting oracle cards")
-if not os.path.exists("oracle_cards.json"):
-    with requests.get(oracle_cards_uri) as r:
-        resp = requests.get(oracle_cards_uri)
-        resp.raise_for_status()
-        oracle_cards = resp.json()
-        with open("oracle_cards.json", 'w') as f:
-            json.dump(oracle_cards, f)
-with open("oracle_cards.json", 'r') as f:
-    oracle_cards = json.load(f)
-
-
-for card in oracle_cards[:50]:
-    if 'Creature' in card['type_line']:
-        print(f"Adding {card['name']}")
-        converted_mana_cost = int(card['cmc'])
-        img_uri = card['image_uris']['normal']
-        card_path = os.path.join("cards", str(converted_mana_cost), card['name']+'.jpg')
-        os.makedirs(os.path.dirname(card_path), exist_ok=True)
-        resp = requests.get(img_uri)
-        resp.raise_for_status()
-        img = Image.open(BytesIO(resp.content))
-        img = img.convert('L').resize((360,int((360/img.width)*img.height)))
-        img.save(card_path)
-"""
