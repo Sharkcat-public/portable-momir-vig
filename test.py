9d0cf92b815851db84d98dfb1250f8106ea5d673
Ellie <erko.cernaj@gmail.com>
2026-03-28 17:25:48 +0100

    buttons

--- test.py
+++ test.py
@@ -1,46 +1,71 @@
 # https://ssd1306.readthedocs.io/en/latest/python-usage.html
+# https://pillow.readthedocs.io/en/latest/reference/ImageDraw.html#module-PIL.ImageDraw
 from luma.oled.device import sh1106
 from luma.core.render import canvas
 from luma.core.interface.serial import i2c
 # https://python-escpos.readthedocs.io/en/latest/
 from escpos.printer import Serial
+from gpiozero import Button
+from signal import pause
 import os
 import random
 import time
 
+# 132x64 sh1106 display
 serial = i2c(port=1, address=0x3C)
 device = sh1106(serial)
 device.show()
 
-valid = False
-while not valid:
-    cost = random.randint(0,20)
-    valid = os.path.exists(os.path.join('cards', str(cost)))
+# 2 buttons
+left_button = Button(17)
+right_button = Button(27)
 
-# https://pillow.readthedocs.io/en/latest/reference/ImageDraw.html#module-PIL.ImageDraw
+current_card = None
+current_cost = 0
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
-    draw.text((5, 20), "Choosing random", fill="white")
-    draw.text((5, 40), f"{cost} cost card", fill="white")
+    draw.text((5, 20), f"Choice: {str(current_cost)} mana", fill="white")
 
-time.sleep(3)
-cards = os.listdir(os.path.join("cards", str(cost)))
-choice = random.randint(0,len(cards)-1)
-chosen_card = cards[choice]
+def up_cost():
+    current_cost += 1
+    if current_cost > 8:
+        current_cost = 0
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((5, 20), f"Choice: {str(current_cost)} mana", fill="white")
 
-with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="red", fill="black")
-    draw.text((5, 40), chosen_card, fill="white")
+left_button.when_pressed = up_cost
+    
+def choose_random_card(cost):
+    # returns path to card img
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((5, 20), "Choosing random", fill="white")
+        draw.text((5, 40), f"{str(cost)} cost card", fill="white")
+    time.sleep(3)
+    cards = os.listdir(os.path.join("cards", str(cost)))
+    choice = random.randint(0,len(cards)-1)
+    chosen_card = cards[choice]
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((5, 40), chosen_card.replace('.jpg',''), fill="white")
+    return chosen_card
 
+def get_card():
+    current_card = choose_random_card(current_cost)
 
-p = Serial(devfile='/dev/serial0',
-           baudrate=9600,
-           bytesize=8,
-           parity='N',
-           stopbits=1,
-           timeout=1.00,
-           dsrdtr=True)
-p.image(os.path.join("cards", str(cost), chosen_card))
+right_button.when_pressed = get_card
 
-time.sleep(3)
-device.hide()
+def print_card(path):
+    p = Serial(devfile='/dev/serial0',
+               baudrate=9600,
+               bytesize=8,
+               parity='N',
+               stopbits=1,
+               timeout=1.00,
+               dsrdtr=True)
+    p.image(path)
+    # 4 newlines to push card out of printer
+    p.text('\n\n\n\n')
+
+pause()
