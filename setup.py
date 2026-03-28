bd78d21840083c088edafb57c4e13cadbd33c92d
Ellie <erko.cernaj@gmail.com>
2026-03-28 16:19:47 +0100

    screen and printer test

--- setup.py
+++ setup.py
@@ -27,10 +27,12 @@ for card in ijson.items(parser, 'item'):
     if i == 10:
         break
     if ('Creature' in card['type_line']) and ('Token' not in card['type_line']):
-        print(f"Adding {card['name']}")
         converted_mana_cost = int(card['cmc'])
         img_uri = card['image_uris']['normal']
         card_path = os.path.join("cards", str(converted_mana_cost), card['name']+'.jpg')
+        if os.path.exists(card_path):
+            continue
+        print(f"Adding {card['name']}")
         os.makedirs(os.path.dirname(card_path), exist_ok=True)
         resp = requests.get(img_uri)
         resp.raise_for_status()
--- test.py
+++ test.py
@@ -2,21 +2,44 @@
 from luma.oled.device import sh1106
 from luma.core.render import canvas
 from luma.core.interface.serial import i2c
+# https://python-escpos.readthedocs.io/en/latest/
+from escpos.printer import Serial
+import os
+import random
+import time
+
 serial = i2c(port=1, address=0x3C)
 device = sh1106(serial)
+device.show()
+
+valid = False
+while not valid:
+    cost = random.randint(0,20)
+    valid = os.path.exists(os.path.join('cards', str(cost)))
+
 # https://pillow.readthedocs.io/en/latest/reference/ImageDraw.html#module-PIL.ImageDraw
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
-    draw.text((30, 40), "Hello World", fill="white")
-    
-# https://python-escpos.readthedocs.io/en/latest/    
-# from escpos.printer import Serial
-# p = Serial(devfile='/dev/serial0',
-#            baudrate=9600,
-#            bytesize=8,
-#            parity='N',
-#            stopbits=1,
-#            timeout=1.00,
-#            dsrdtr=True)
-# p.text("Hello World\n")
-# p.image("cards/4/Nissa, Worldsoul Speaker.jpg")
+    draw.text((5, 40), f"Choosing random {cost} cost card", fill="white")
+
+time.sleep(3)
+cards = os.listdir(os.path.join("cards", str(cost)))
+choice = random.randint(0,len(cards)-1)
+chosen_card = cards[choice]
+
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="red", fill="black")
+    draw.text((5, 40), chosen_card, fill="white")
+
+
+p = Serial(devfile='/dev/serial0',
+           baudrate=9600,
+           bytesize=8,
+           parity='N',
+           stopbits=1,
+           timeout=1.00,
+           dsrdtr=True)
+p.image(os.path.join("cards", str(cost), chosen_card))
+
+time.sleep(3)
+device.hide()
