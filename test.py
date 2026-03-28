0cb8202e41e4b45b91d7746e372999dbd7b5bb8c
Ellie <erko.cernaj@gmail.com>
2026-03-28 19:45:42 +0100

    4 button setup

--- test.py
+++ test.py
@@ -12,26 +12,11 @@ import os
 import random
 import time
 
-print('display setup')
-# 132x64 sh1106 display
-serial = i2c(port=1, address=0x3C)
-device = sh1106(serial)
-device.show()
-
-print('button setup')
-# 2 buttons
-left_button = Button(17, bounce_time=0.3)
-right_button = Button(27, bounce_time=0.3)
-
-available_cmc = []
-for c in os.listdir('cards'):
-    available_cmc.append(int(c))
-available_cmc.sort()
-current_card = None
-current_cost = available_cmc[0]
-with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="black")
-    draw.text((5, 20), f"Choice: {str(current_cost)} mana", fill="white")
+def display_mana_choice():
+    global current_cost
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((5, 20), f"Choice: {str(current_cost)} mana", fill="white")
 
 def up_cost():
     global current_cost
@@ -41,35 +26,34 @@ def up_cost():
     if current_index >= len(available_cmc):
         current_index = 0
     current_cost = available_cmc[current_index]
-    with canvas(device) as draw:
-        draw.rectangle(device.bounding_box, outline="white", fill="black")
-        draw.text((5, 20), f"Choice: {str(current_cost)} mana", fill="white")
 
-left_button.when_pressed = up_cost
-    
-def choose_random_card(cost):
+def down_cost():
+    global current_cost
+    global available_cmc
+    current_index = available_cmc.index(current_cost)
+    current_index -= 1
+    if current_index < 0:
+        current_index = len(available_cmc) - 1
+    current_cost = available_cmc[current_index]
+
+def roll_random_card():
+    global current_cost
+    global current_card
     # returns path to card img
     with canvas(device) as draw:
         draw.rectangle(device.bounding_box, outline="white", fill="black")
         draw.text((5, 20), "Choosing random", fill="white")
-        draw.text((5, 40), f"{str(cost)} cost card", fill="white")
-    time.sleep(3)
-    cards = os.listdir(os.path.join("cards", str(cost)))
+        draw.text((5, 40), f"{str(current_cost)} cost card", fill="white")
+    time.sleep(2)
+    cards = os.listdir(os.path.join("cards", str(current_cost)))
     choice = random.randint(0,len(cards)-1)
-    chosen_card = cards[choice]
+    current_card = cards[choice]
     with canvas(device) as draw:
         draw.rectangle(device.bounding_box, outline="white", fill="black")
-        draw.text((5, 40), chosen_card.replace('.jpg',''), fill="white")
-    return chosen_card
+        draw.text((5, 40), current_card.replace('.jpg',''), fill="white")
 
-def get_card():
+def print_card():
     global current_card
-    current_card = choose_random_card(current_cost)
-    print_card(os.path.join('cards',str(current_cost),current_card))
-
-right_button.when_pressed = get_card
-
-def print_card(path):
     p = Serial(devfile='/dev/serial0',
                baudrate=9600,
                bytesize=8,
@@ -77,9 +61,76 @@ def print_card(path):
                stopbits=1,
                timeout=1.00,
                dsrdtr=True)
-    p.image(path)
+    p.image(os.path.join('cards',str(current_cost),current_card))
     # 4 newlines to push card out of printer
     p.text('\n\n\n\n')
+
+def left_button_func():
+    global current_state
+    if current_state == 0:
+        down_cost()
+        display_mana_choice()
+    if current_state == 1:
+        roll_random_card()
+        
+
+def right_button_func():
+    global current_state
+    if current_state == 0:
+        up_cost()
+        display_mana_choice()
+    if current_state == 1:
+        roll_random_card()
+
+def accept_button_func():
+    global current_state
+    if current_state == 0:
+        roll_random_card()
+        current_state += 1
+    if current_state == 1:
+        current_state += 1
+        print_card()
+        current_state = 0
+
+def back_button_func():
+    global current_state
+    if current_state == 0:
+        #TODO: os.shutdown???
+        pass
+    if current_state == 1:
+        current_state -= 1
+        display_mana_choice()
+
+print('display setup')
+# 132x64 sh1106 display
+serial = i2c(port=1, address=0x3C)
+device = sh1106(serial)
+device.show()
+
+print('button setup')
+# 4 buttons
+left_button = Button(17, bounce_time=0.01)
+right_button = Button(27, bounce_time=0.01)
+accept_button = Button(22, bounce_time=0.01)
+back_button = Button(23, bounce_time=0.01)
+left_button.when_pressed = left_button_func
+right_button.when_pressed = right_button_func
+accept_button.when_pressed = accept_button_func
+back_button.when_pressed = back_button_func
+
+available_cmc = []
+for c in os.listdir('cards'):
+    available_cmc.append(int(c))
+available_cmc.sort()
+
+# states:
+# 0: mana choice
+# 1: card gen
+# 2: card print
+current_state = 0
+current_card = None
+current_cost = available_cmc[0]
+display_mana_choice()
 
 print('ready')
 pause()
